a6d5f885a4f8de19c63e071326fe9d450371764b
wajih_52 <wajih.rjeb@esprit.tn>
2022-04-12 14:35:03 +0100

    essai

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VehiculeComponent } from './vehicule/vehicule.component';
+import {FormsModule} from "@angular/forms";
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VehiculeComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
